71ae590fe198e28d53de8b679cfb1c182797e020
Rolan <rolandrenier@hotmail.com>
2026-01-26 16:50:26 +0100

    modifs mineures tests en défau car pas de BDD en CI_V5

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,12 +11,13 @@ from pathlib import Path
 import pytest
 from fastapi.testclient import TestClient
 
-from api_app.main import app
-
-
+# IMPORTANT : on ajoute d'abord la racine du projet au PYTHONPATH
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
+
+# Ensuite seulement, on peut importer l'app
+from api_app.main import app  # noqa: E402
 
 
 @pytest.fixture
